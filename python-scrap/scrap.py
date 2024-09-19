559a9c96f352c4e28054ba46eb272e13c74fb877
lvininicius <lvininicius@gmail.com>
2024-09-19 15:21:22 -0300

    adicionando alterações e objeto items

--- python-scrap/scrap.py
+++ python-scrap/scrap.py
@@ -1,14 +1,25 @@
-import scrapy
+import sys,os
+sys.path.append(os.path.dirname(__file__))
+sys.path.append("./venv/lib/python3.11/site-packages/scrapy/templates/project/module")
+sys.path.append("items.py")
+from scrapy import Spider
+from items import MovieItem, MovieItemLoader
 
-class AdoroCinemaSpider(scrapy.Spider):
+class AdoroCinemaSpider(Spider):
     name = 'adorocinema'
     start_urls = ['https://www.adorocinema.com/filmes/melhores/']
 
     def parse(self, response):
         movies = response.xpath("//div[contains(@class,'card-list')]")
         for movie in movies:
-            yield {
-                "title": movie.css("a.meta-title-link::text").get(),
-                "duration": movie.css("div.meta-body-item.meta-body-info::text").get(default='').replace(" ", ""),
-                "original_title": movie.xpath("div[1]//div[@class='meta-body-item']/span[2]/text()").get(),
-            }
+            title = movie.css("a.meta-title-link::text").get()
+            duration = movie.css("div.meta-body-item.meta-body-info::text").get().replace("\n", "")
+            original_title = movie.xpath("div[1]//div[@class='meta-body-item']/span[2]/text()").getall()[-1]
+            
+
+            il=MovieItemLoader(item=MovieItem(), selector=movies)
+            il.add_value("title",title)
+            il.add_value("duration",duration)
+            il.add_value("original_title",original_title)
+
+            yield il.load_item()
